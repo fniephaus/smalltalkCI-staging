740c5dfdf48c620b219e4933b6693bef202d6f12
Fabio Niephaus <code@fniephaus.com>
2016-11-21 17:50:24 +0100

    Ping travis

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ curl -f -L --retry 3 -o cog.tar.gz https://dl.bintray.com/opensmalltalk/vm/cog_l
 curl -f -L --retry 3 -o image.tar.gz https://dl.bintray.com/hpi-swa-lab/smalltalkCI/Squeak-5.1.tar.gz
 tar xzf cog.tar.gz
 tar xzf image.tar.gz
+
 echo "VM..."
 ./products/sqcogspurlinuxht/squeak -version
 echo "Run..."
