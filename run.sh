1f5e97e3458bdca9226c2b35e6454697fe5886b5
Fabio Niephaus <code@fniephaus.com>
2016-11-21 17:09:08 +0100

    Test ht cog vm

---
+++ run.sh
@@ -0,0 +1,3 @@
+curl -f -L --retry 3 -o cog.tar.gz https://dl.bintray.com/opensmalltalk/vm/cog_linux32x86_squeak.cog.spur_201611162256.tar.gz
+tar xzvf cog.tar.gz
+./products/sqcogspurlinuxht/squeak -version
