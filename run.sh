71b9fe0046fa2ff59046a68e8ee2d8b28fcfdbc5
Fabio Niephaus <code@fniephaus.com>
2016-11-21 17:27:31 +0100

    Add test script

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 curl -f -L --retry 3 -o cog.tar.gz https://dl.bintray.com/opensmalltalk/vm/cog_linux32x86_squeak.cog.spur_201611162256.tar.gz
-tar xzvf cog.tar.gz
+curl -f -L --retry 3 -o image.tar.gz https://dl.bintray.com/hpi-swa-lab/smalltalkCI/Squeak-5.1.tar.gz
+tar xzf cog.tar.gz
+tar xzf image.tar.gz
+echo "VM..."
 ./products/sqcogspurlinuxht/squeak -version
+echo "Run..."
+./products/sqcogspurlinuxht/squeak ./Travis.image "$(pwd)/script.st"
